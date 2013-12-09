feacfe106348643968a1e1962c5831acef6cfd0b
aCandidMind <opensource@candidminds.com>
2013-12-09 07:23:27 +0100

    Serializing spots compliant to the GeoJSON standard

--- app/serializers/spot_serializer.rb
+++ app/serializers/spot_serializer.rb
@@ -1,8 +1,33 @@
+# Serializing spots compliant to the GeoJSON standard
+# @see http://geojson.org/geojson-spec.html
 class SpotSerializer < ActiveModel::Serializer
-  attributes :id, :location, :name, :description
+  attributes :id, :type, :properties, :geometry
 
   # Making sure MongoDB ID's aren't supported with $oid but as simple strings instead
+  # @return MongoDB ID [String]
   def id
     object.id.to_s
+  end
+
+  # @see http://geojson.org/geojson-spec.html#feature-objects
+  # @return the GeoJSON object type "Feature" [String]
+  def type
+    "Feature"
+  end
+
+  def properties
+    {name: object.name, description: object.description}
+  end
+
+  # @see http://geojson.org/geojson-spec.html#geometry-objects
+  # @return GeoJSON geometry [Hash]
+  def geometry
+    {type: geometry_type, coordinates: object.location}
+  end
+
+  # @see http://geojson.org/geojson-spec.html#point
+  # @return GeoJSON geometry type "Point" [String]
+  def geometry_type
+    "Point"
   end
 end
