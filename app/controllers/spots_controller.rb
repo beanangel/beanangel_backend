939f2bb96c603f58228ffe49aa5b91d3e1c77dc8
aCandidMind <opensource@candidminds.com>
2013-12-09 06:37:51 +0100

    Allowing CORS on locahost:9000 as that's our frontend.

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,5 +1,21 @@
 class SpotsController < ApplicationController
+
+  before_action :allow_cors
+
   def index
     render json: Spot.all
   end
+
+  private
+
+    def allow_cors
+      headers["Access-Control-Allow-Origin"] = "http://localhost:9000"
+      headers["Access-Control-Allow-Methods"] = 'GET, POST, PUT, DELETE'
+      headers["Access-Control-Allow-Headers"] =
+      'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+      head(:ok) if request.request_method == "OPTIONS"
+      # or, render text: ''
+      # if that's more your style
+    end
 end
