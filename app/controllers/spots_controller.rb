23d59d02e8196bbdc593458b409a43135bdf752a
aCandidMind <opensource@candidminds.com>
2013-12-11 01:44:20 +0100

    SpotsController CORS Allow-Origin set to http://staging2.united-creative.com (omitting the port).

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -9,7 +9,7 @@ class SpotsController < ApplicationController
   private
 
     def allow_cors
-      origin = Rails.env.production? ? "http://staging2.united-creative.com:9000" : 'http://localhost:9000'
+      origin = Rails.env.production? ? "http://staging2.united-creative.com" : 'http://localhost:9000'
       headers["Access-Control-Allow-Origin"] = origin
       headers["Access-Control-Allow-Methods"] = 'GET, POST, PUT, DELETE'
       headers["Access-Control-Allow-Headers"] =
