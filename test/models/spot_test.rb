ba54e392599b9d5804b364afcd0fcd606b478b2b
aCandidMind <opensource@candidminds.com>
2013-12-16 00:36:49 +0100

    Fixing SpotTest which wasn't failing when it should.

--- test/models/spot_test.rb
+++ test/models/spot_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class SpotTest < ActiveSupport::TestCase
-  test "doesn't save without a location" do
-    spot = Spot.new
-    spot.location = nil
-    assert !spot.save, "shouldn't save without a location"
+
+  setup do
+    @spots = YAML.load_file('test/fixtures/spots.yml')
   end
+
+  required_fields = [:location]
+
+  required_fields.each do |field|
+    test "doesn't save without a #{field}" do
+      spot = Spot.new(@spots['standard'])
+      spot.send("#{field}=", nil)
+      assert !spot.save, "shouldn't save without a #{field}"
+    end
+  end
+
 end
