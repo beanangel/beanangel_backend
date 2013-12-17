3ad6db79319249865f19b5b70373afdcd8d64f77
aCandidMind <opensource@candidminds.com>
2013-12-17 08:41:32 +0100

    When a request came from jquery.iframe-transport we render HTML in the way they propose.

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,21 +1,49 @@
 class SpotsController < ApplicationController
 
+  include ActionController::MimeResponds # for respond_to, remove this once jquery.iframe-transport is gone
+
   def index
     render json: Spot.all
   end
 
   def create
     @spot = Spot.create(spot_params)
-    render json: @spot
+    respond_to do |format|
+      # html is only for jquery.iframe-transport, once that is gone, remove this
+      format.html { render_for_iframe_transport }
+      format.any(:js, :json) {
+        render json: @spot
+      }
+    end
   end
 
   def update
     @spot = Spot.find(params[:id])
     @spot.update_attributes(spot_params)
-    render json: @spot
+    respond_to do |format|
+      # html is only for jquery.iframe-transport, once that is gone, remove this
+      format.html { render_for_iframe_transport }
+      format.any(:js, :json) {
+        render json: @spot
+      }
+    end
   end
 
   private
+
+    # This is only for jquery.iframe-transport, once that is gone, remove this.
+    # Check the following link to see, why we have a special rendering for this jquery transport
+    # @see http://cmlenz.github.io/jquery-iframe-transport/#section-9
+    def render_for_iframe_transport
+      # Check whether this comes from jquery.iframe-transport
+      # Eventually we're only supporting application/json so we except X-HTTP-Accept to be just that
+      if  headers['X-Requested-With'] == 'IFrame' &&
+          headers['X-HTTP-Accept'].starts_with?("application/json")
+        render action_name, format: :html
+      else
+        render nothing: true
+      end
+    end
 
     def spot_params
       attrs = params[:spot].dup
